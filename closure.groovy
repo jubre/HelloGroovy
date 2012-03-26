c361c42ffce4119718c452521962d634a9b42795
Junior Corman <junior.corman@gmail.com>
2012-03-26 18:05:05 -0500

    add some examples about clousure

--- closure.groovy
+++ closure.groovy
@@ -15,8 +15,31 @@ value.each{ print it + '-' }
 
 // find
 def finded = [1, 2, 3].find { it > 1 }
-print '\n' + finded
+println '\n' + finded
 
 // findAll
 def findedAll = [1, 2, 3].findAll { it > 1 }
-print '\n' + findedAll
+println findedAll
+
+// inject
+def injectable = [1, 2, 3].inject(0){ count, item -> count + item }
+println injectable
+
+// every: returns true if all items match the clousure predictable
+def resultEvery = [1, 2, 3].every { it < 5 }
+println resultEvery
+
+// any: returns true if any item match the clousure predictable
+def resultAny = [1, 2, 3].any { it < 5 }
+println resultAny
+
+// max/min: returns the max/min value of the collection - for comparable Objects
+value = [9, 4, 2, 10, 5].max()
+println '[9, 4, 2, 10, 5].max() = ' + value
+value = [9, 4, 2, 10, 5].min()
+println '[9, 4, 2, 10, 5].min() = ' + value
+
+// join: concatenates the values of the collection together with a string value
+value = [1, 2, 3].join('-')
+println value
+
